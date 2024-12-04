10d0ebfc2dce5d1ffdb8daf7c6a188fc18d0c149
alynaug <alyn.a@hotmail.com>
2024-12-04 22:56:11 +0700

    OAuth connection

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,16 @@
 import type { NextAuthConfig } from "next-auth";
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    Google,
+    Github,
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -21,8 +21,15 @@ import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider!"
+      : "";
+
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -95,7 +102,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
             Login
